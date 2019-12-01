ac515b29f6782f650ae820e218f5250d0dceb92e
reyesj5 <reyesj5@uchicago.edu>
2019-12-01 16:21:26 -0600

    working on some restructuring

--- moodzaic_django/mood_model/models.py
+++ moodzaic_django/mood_model/models.py
@@ -27,99 +27,6 @@ class Weights(models.Model):
         default=""
     )
 
-    def getData(self, obs, observations, timeframe):
-        today = obs.date.today()
-        week_ago = today - datetime.timedelta(days=timeframe)
-        weekly_data = observations.filter(date__gte=week_ago, date__lte=today)
-
-        days_with_obs = weekly_data.__len__
-        if days_with_obs == 0:
-            return 0, 0
-
-        exercise_sum, work_sum = 0, 0
-
-        for past_obs in weekly_data.iterator():
-            exercise_sum += past_obs.exercise
-            work_sum += past_obs.work
-
-        return exercise_sum, work_sum
-        # return exercise_sum / days_with_obs, work_sum / days_with_obs
-        # will consider averages in the future as a data point
-
-
-    def transformUserData(self, timeframe):
-        profile = self.user.profile
-        observations = Observation.objects.filter(user__user__username=profile.user.username)
-        observations = observations.order_by("date")
-
-        input_data = []
-        mood_data = []
-
-        if len(observations) > 0:
-            today = observations[0].date.today()
-            timeframe_ago = today - datetime.timedelta(days=timeframe)
-            timeframe_data = observations.filter(date__gte=timeframe_ago, date__lte=today)
-
-
-            for obs in timeframe_data.iterator():
-                weekly_exercise, weekly_work = self.getData(obs, observations, 7)
-
-                row = [
-                    obs.sleep, obs.exercise, weekly_exercise, obs.meals,
-                    obs.numberOfGoals, obs.goalsCompleted, obs.goalsMissed,
-                    obs.goalsRatio, obs.pastMoodScore,
-                    obs.work, weekly_work
-                ]
-                input_data.append(row)
-                mood_data.append(obs.mood)
-
-        return np.array(input_data), np.array(mood_data)
-
-
-
-    def getWeightBiasDictionaries(self):
-        weights = self.weights_int_list.split(',')
-        biases = self.bias_int_list.split(',')
-        weightDict = {}
-        biasDict = {}
-        for i in range(len(weights)):
-            if i < 21:
-                biasDict['bias' + str(i)] = float(biases[i])
-            weightDict['weight' + str(i)] = float(weights[i])
-        return weightDict, biasDict
-
-    def retrain(self):
-        weightDict, biasDict = self.getWeightBiasDictionaries()
-        model = MoodNeuralNetwork(weights=weightDict, biases=biasDict)
-        input_data, mood_data = self.transformUserData(7)
-        model.train(input_data, mood_data)
-        weightDict = model.getWeights()
-        weights = []
-        for i in range(len(weightDict)):
-            weights.append(weightDict["weight" + str(i)])
-        self.setWeightsWeights(weights)
-
-        biasDict = model.getBiases()
-        biases = []
-        for i in range(len(biasDict)):
-            biases.append(biasDict["bias" + str(i)])
-        self.setWeightsBias(biases)
-
-        return True
-
-    def predict(self):
-        weightDict, biasDict = self.getWeightBiasDictionaries()
-        model = MoodNeuralNetwork(weights=weightDict, biases=biasDict)
-        input_data, mood_data = self.transformUserData(1)
-        try:
-            output = model.feedforward(input_data[0])
-        except:
-            return -1
-        mood = model.roundClass(output)
-        self.updateMoodPrediction(mood)
-        return mood
-        #self.user.profile.setNotifications()
-
     def setWeightsUser(self, user):
         if self.user is not None:
             return False
@@ -156,6 +63,97 @@ class Weights(models.Model):
             self.setWeightsBias(biases)
         return True
 
+    def getWeightBiasDictionaries(self):
+        weights = self.weights_int_list.split(',')
+        biases = self.bias_int_list.split(',')
+        weightDict = {}
+        biasDict = {}
+        for i in range(len(weights)):
+            if i < 21:
+                biasDict['bias' + str(i)] = float(biases[i])
+            weightDict['weight' + str(i)] = float(weights[i])
+        return weightDict, biasDict
+
+    def getData(self, obs, observations, timeframe):
+        today = obs.date
+        timeframe_ago = today - datetime.timedelta(days=timeframe)
+        timeframe_data = observations.filter(date__gte=timeframe_ago, date__lte=today)
+
+        days_with_obs = timeframe_data.__len__
+        if days_with_obs == 0:
+            return 0, 0
+
+        exercise_sum, work_sum = 0, 0
+
+        for past_obs in timeframe.iterator():
+            exercise_sum += past_obs.exercise
+            work_sum += past_obs.work
+
+        return exercise_sum, work_sum
+        # return exercise_sum / days_with_obs, work_sum / days_with_obs
+        # will consider averages in the future as a data point
+
+
+    def transformUserData(self, timeframe):
+        self.updateLongtermData(timeframe)
+        profile = self.user.profile
+        observations = Observation.objects.filter(user__user__username=profile.user.username)
+        observations = observations.order_by("date")
+
+        input_data = []
+        mood_data = []
+
+        if len(observations) > 0:
+            today = observations[0].date.today()
+            timeframe_ago = today - datetime.timedelta(days=timeframe)
+            timeframe_data = observations.filter(date__gte=timeframe_ago, date__lte=today)
+
+
+            for obs in timeframe_data.iterator():
+
+                row = [
+                    obs.sleep, obs.exercise, self.weeklyExercise, obs.meals,
+                    obs.numberOfGoals, obs.goalsCompleted, obs.goalsMissed,
+                    obs.goalsRatio, obs.pastMoodScore,
+                    obs.work, self.weeklyWork
+                ]
+                input_data.append(row)
+                mood_data.append(obs.mood)
+
+        return np.array(input_data), np.array(mood_data)
+
+    def retrain(self):
+        weightDict, biasDict = self.getWeightBiasDictionaries()
+        model = MoodNeuralNetwork(weights=weightDict, biases=biasDict)
+        input_data, mood_data = self.transformUserData(30)
+        model.train(input_data, mood_data)
+        weightDict = model.getWeights()
+        weights = []
+        for i in range(len(weightDict)):
+            weights.append(weightDict["weight" + str(i)])
+        self.setWeightsWeights(weights)
+
+        biasDict = model.getBiases()
+        biases = []
+        for i in range(len(biasDict)):
+            biases.append(biasDict["bias" + str(i)])
+        self.setWeightsBias(biases)
+
+        return True
+
+    def predict(self):
+        weightDict, biasDict = self.getWeightBiasDictionaries()
+        model = MoodNeuralNetwork(weights=weightDict, biases=biasDict)
+        input_data, mood_data = self.transformUserData(1)
+        try:
+            output = model.feedforward(input_data[0])
+        except:
+            return -1
+        mood = model.roundClass(output)
+        self.updateMoodPrediction(mood)
+        return mood
+        #self.user.profile.setNotifications()
+
     def updateLongtermData(self, timeframe):
         try:
             profile = self.user.profile
@@ -176,7 +174,6 @@ class Weights(models.Model):
             return False
         except:
             return False
-
 
     def updateMoodPrediction(self, prediction):
         profile = self.user.profile
